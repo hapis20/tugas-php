1ce8caa3e48d5fba93ac2413e0478900c2dafd91
hapis20 <114800847+hapis20@users.noreply.github.com>
2022-12-21 20:45:46 +0700

    tugas-php

---
+++ latihan2.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <title></title>
+</head>
+
+<body>
+    <h2> Ganjil 0 s.d 6</h2>
+    <?php
+
+    for ($i = 1; $i <= 20; $i++) {
+
+        if ($i % 2 == 0) {
+
+            echo $i . " ";
+        }
+    }
+
+
+    ?>
+</body>
+
+</html>
